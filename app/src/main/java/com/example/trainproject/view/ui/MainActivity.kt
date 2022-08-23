8a17f6d9fcbf0490b42c6b125580ad7baf73b454
Raneemq1 <raneemalqadi32@gmail.com>
2022-08-23 12:59:28 +0300

    second commit

--- app/src/main/java/com/example/trainproject/view/ui/MainActivity.kt
+++ app/src/main/java/com/example/trainproject/view/ui/MainActivity.kt
@@ -6,10 +6,12 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.example.trainproject.R
 import com.example.trainproject.data.model.PostModel
 import com.example.trainproject.view.adapter.PostRecyclerAdapter
 import com.example.trainproject.viewmodel.PostViewModel
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,6 +25,11 @@ class MainActivity : AppCompatActivity() {
      */
     private lateinit var postRecycler: RecyclerView
     private lateinit var postAdapter: PostRecyclerAdapter
+
+    /**
+     * Initialize swipe refresh to notify recycler adapter when data updated
+     */
+    private lateinit var swipeRefresh :SwipeRefreshLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,12 +49,29 @@ class MainActivity : AppCompatActivity() {
         postAdapter = PostRecyclerAdapter()
         postRecycler.adapter = postAdapter
 
+        swipeRefresh=findViewById(R.id.swipeRefresh)
+
         /**
          * Observe mutable live data in view model to update the list in the recycler adapter
          */
         viewModel.data.observe(this, Observer<List<PostModel>>{list->
             postAdapter.postsList=list
         })
+
+
+        swipeRefresh.setOnRefreshListener {
+
+            // on below line we are setting is refreshing to false.
+            swipeRefresh.isRefreshing=false
+
+            /**
+             * Shuffle the data to simulate data updated process
+             */
+            Collections.shuffle(postAdapter.postsList, Random(System.currentTimeMillis()))
+
+            postAdapter.notifyDataSetChanged()
+
+        }
 
 
     }
